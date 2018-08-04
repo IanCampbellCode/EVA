ceb6e450eddf63b21728a414229185d8b3b6e4e4
Ian Campbell <iancampbellcode@gmail.com>
2018-08-04 13:30:44 -0400

    Added a pre-build script that copies repositories.toml to the release folders. This is unoptimized and doesn't check if they exist ahead of time, it just goes and does it.

--- build.rs
+++ build.rs
@@ -1,9 +1,12 @@
 use std::fs;
 
+///An under-optimized pre-build script which copies the repositories file from src to the target folders
 fn main() {
-    fs::create_dir_all("/target/debug/resources").expect("Could not create /target/debug/resources");
-    fs::create_dir_all("/target/release/resources").expect("Could not create /target/release/resources");
+    //Create resource folders if they do not exist
+    fs::create_dir_all("target/debug/resources").expect("Could not create /target/debug/resources");
+    fs::create_dir_all("target/release/resources").expect("Could not create /target/release/resources");
 
-    fs::copy("/src/resources/repositories.toml", "/target/debug/resources").expect("could not copy repositories file to debug");
-    fs::copy("/src/resources/repositories.toml", "/target/release/resources").expect("could not copy repositories file to release");
+    //Copy repository file from source to target folders (Even if the exist because this isn't optimized)
+    fs::copy("src/resources/repositories.toml", "target/debug/resources/repositories.toml").expect("could not copy repositories file to debug");
+    fs::copy("src/resources/repositories.toml", "target/release/resources/repositories.toml").expect("could not copy repositories file to release");
 }
